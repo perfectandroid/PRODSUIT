d1cebb65e9d97bedd30c0c3de30cc33861ec6bf7
perfectandroid <psstechteam@gmail.com>
2022-03-03 12:44:42 +0530

    Added sidemenu activities .

---
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/ContactUsActivity.kt
@@ -0,0 +1,12 @@
+package com.perfect.prodsuit.View.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.perfect.prodsuit.R
+
+class ContactUsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_contact_us)
+    }
+}
---
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/ProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.perfect.prodsuit.View.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.perfect.prodsuit.R
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
