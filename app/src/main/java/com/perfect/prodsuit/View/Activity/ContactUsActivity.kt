2dd89f5fba4267a0c4753e23070e34ae44d72ed9
perfectandroid <psstechteam@gmail.com>
2022-03-04 15:04:53 +0530

    Added designs of contactus, changempin logout, quit.

--- app/src/main/java/com/perfect/prodsuit/View/Activity/ContactUsActivity.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/ContactUsActivity.kt
@@ -1,14 +1,22 @@
 package com.perfect.prodsuit.View.Activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
-import android.widget.TextView
-import com.perfect.prodsuit.BuildConfig
+import androidx.appcompat.app.AppCompatActivity
 import com.perfect.prodsuit.R
 
 class ContactUsActivity : AppCompatActivity() , View.OnClickListener {
+
+    private var et_name: EditText? = null
+    private var et_subject: EditText? = null
+    private var et_msg: EditText? = null
+    private var imback: ImageView? = null
+    private var btnOk: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_us)
@@ -16,9 +24,15 @@ class ContactUsActivity : AppCompatActivity() , View.OnClickListener {
     }
 
     private fun setRegViews() {
-        val imback = findViewById<ImageView>(R.id.imback) as ImageView
-        imback!!.setOnClickListener(this)
 
+        et_name = findViewById<EditText>(R.id.et_name)
+        et_subject = findViewById<EditText>(R.id.et_subject)
+        et_msg = findViewById<EditText>(R.id.et_msg)
+        imback = findViewById<ImageView>(R.id.imback)
+        btnOk = findViewById<Button>(R.id.btnOk)
+
+        btnOk!!.setOnClickListener(this)
+        imback!!.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -26,6 +40,42 @@ class ContactUsActivity : AppCompatActivity() , View.OnClickListener {
             R.id.imback->{
                 finish()
             }
+            R.id.btnOk->{
+                validation()
+                et_msg!!.setText("")
+                et_name!!.setText("")
+                et_subject!!.setText("")
+            }
         }
     }
+
+    private fun sendMail(subject: String, msg: String)
+    {
+        val to = "pssappfeedback@gmail.com"
+        val subject = subject
+        val message = msg
+
+        val intent = Intent(Intent.ACTION_SEND)
+        val addressees = arrayOf(to)
+        intent.putExtra(Intent.EXTRA_EMAIL, addressees)
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+        intent.putExtra(Intent.EXTRA_TEXT, message)
+        intent.setType("message/rfc822")
+        startActivity(Intent.createChooser(intent, "Send Email using:"))
+    }
+
+    private fun validation(){
+        if (et_name!!.text.toString() == null || et_name!!.text.toString().isEmpty()) {
+            et_name!!.setError("Please Enter Your Name.")
+        }
+        else if (et_subject!!.text.toString() == null || et_subject!!.text.toString().isEmpty()) {
+            et_subject!!.setError("Please Enter Your Subject.")
+        }
+        else if (et_msg!!.text.toString() == null || et_msg!!.text.toString().isEmpty()) {
+            et_msg!!.setError("Please Enter Your Message.")
+        }else{
+            sendMail("ProdSuit - "+et_subject!!.text.toString(),et_msg!!.text.toString()+"\n\n"+et_name!!.text.toString())
+        }
+    }
+
 }
--- app/src/main/java/com/perfect/prodsuit/View/Activity/HomeActivity.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/HomeActivity.kt
@@ -1,15 +1,23 @@
 package com.perfect.prodsuit.View.Activity
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
+import android.graphics.Typeface
+import android.os.Build
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.text.SpannableStringBuilder
+import android.text.style.TypefaceSpan
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.perfect.prodsuit.Helper.Config
 import com.perfect.prodsuit.R
 
 
@@ -57,6 +65,7 @@ class HomeActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
             R.id.nav_profile -> {
             }
             R.id.nav_changempin -> {
+                changeMpin()
             }
             R.id.nav_about -> {
                 val i = Intent(this@HomeActivity, AboutUsActivity::class.java)
@@ -66,14 +75,21 @@ class HomeActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
                 val i = Intent(this@HomeActivity, ContactUsActivity::class.java)
                 startActivity(i)
             }
-            R.id.nav_rateus -> {
-            }
             R.id.nav_share -> {
+                val shareIntent = Intent()
+                shareIntent.action = Intent.ACTION_SEND
+                shareIntent.type = "text/plain"
+                shareIntent.putExtra(
+                    Intent.EXTRA_TEXT,
+                    getString(R.string.app_name) + "\t" + "Invite You  \n\n For Android Users \n\n http://play.google.com/store/apps/details?id=" + getPackageName() + "\n"
+                )
+                startActivity(Intent.createChooser(shareIntent, "Invite this App to your friends"))
             }
             R.id.nav_logout -> {
+                doLogout()
             }
             R.id.nav_quit -> {
-
+                quit()
             }
         }
         drawer_layout!!.closeDrawer(GravityCompat.START)
@@ -92,6 +108,123 @@ class HomeActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun doLogout() {
+        try {
+            val dialog1 = Dialog(this)
+            dialog1 .requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog1 .setCancelable(false)
+            dialog1 .setContentView(R.layout.logout_popup)
+            dialog1.window!!.attributes.gravity = Gravity.BOTTOM;
+
+            val btn_Yes = dialog1 .findViewById(R.id.btnYes) as Button
+            val btn_No = dialog1 .findViewById(R.id.btnNo) as Button
+            btn_No.setOnClickListener {
+                dialog1 .dismiss()
+            }
+            btn_Yes.setOnClickListener {
+                dialog1.dismiss()
+                dologoutchanges()
+                startActivity(Intent(this@HomeActivity, WelcomeActivity::class.java))
+            }
+            dialog1.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun dologoutchanges() {
+        val loginSP = applicationContext.getSharedPreferences(Config.SHARED_PREF, 0)
+        val loginEditer = loginSP.edit()
+        loginEditer.putString("loginsession", "No")
+        loginEditer.commit()
+    }
+
+    private fun quit() {
+        try {
+            val dialog1 = Dialog(this)
+            dialog1 .requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog1 .setCancelable(false)
+            dialog1 .setContentView(R.layout.quit_popup)
+            dialog1.window!!.attributes.gravity = Gravity.BOTTOM;
+            val btn_Yes = dialog1 .findViewById(R.id.btn_Yes) as Button
+            val btn_No = dialog1 .findViewById(R.id.btn_No) as Button
+            btn_No.setOnClickListener {
+                dialog1 .dismiss()
+            }
+            btn_Yes.setOnClickListener {
+                dialog1.dismiss()
+                finish()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    finishAffinity()
+                }
+            }
+            dialog1.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun changeMpin() {
+        try {
+            val dialog1 = Dialog(this)
+            dialog1 .requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog1 .setCancelable(true)
+            dialog1 .setContentView(R.layout.changepin_popup)
+           // dialog1.window!!.attributes.gravity = Gravity.BOTTOM;
+            val btnreset = dialog1 .findViewById(R.id.btnreset) as Button
+            val btnSubmit = dialog1 .findViewById(R.id.btnSubmit) as Button
+
+            val etxt_oldpin = dialog1 .findViewById(R.id.etxt_oldpin) as EditText
+            val etxt_newpin = dialog1 .findViewById(R.id.etxt_newpin) as EditText
+            val etxt_confirmnewpin = dialog1 .findViewById(R.id.etxt_confirmnewpin) as EditText
+            btnSubmit.setOnClickListener {
+
+                if (etxt_oldpin!!.text.toString() == null || etxt_oldpin!!.text.toString().isEmpty()) {
+                    etxt_oldpin!!.setError("Please Enter Your Old mPin.")
+                }
+                else if (etxt_newpin!!.text.toString() == null || etxt_newpin!!.text.toString().isEmpty()) {
+                    etxt_newpin!!.setError("Please Enter Your New mPin.")
+                }
+                else if (etxt_confirmnewpin!!.text.toString() == null || etxt_confirmnewpin!!.text.toString().isEmpty()) {
+                    etxt_confirmnewpin!!.setError("Please Confirm Your New mPin.")
+                }
+                else if (etxt_newpin!!.text.toString() != etxt_confirmnewpin!!.text.toString()) {
+                    val dialog: AlertDialog.Builder = AlertDialog.Builder(this)
+                    dialog.setMessage("New & Confirm mPin doesn't match")
+                    dialog.setPositiveButton("Ok",
+                        DialogInterface.OnClickListener { dialog, which ->
+                        })
+                    val alertDialog: AlertDialog = dialog.create()
+                    alertDialog.show()
+                }
+                else if (etxt_oldpin!!.text.toString().isNotEmpty() && etxt_oldpin!!.text.toString().length!=6) {
+                    etxt_oldpin.setError("Please Enter 6 digit mPin")
+                }
+                else if (etxt_newpin!!.text.toString().isNotEmpty() && etxt_newpin!!.text.toString().length!=6) {
+                    etxt_newpin.setError("Please Enter 6 digit mPin")
+                }
+                else if (etxt_confirmnewpin!!.text.toString().isNotEmpty() && etxt_confirmnewpin!!.text.toString().length!=6) {
+                    etxt_confirmnewpin.setError("Please Enter 6 digit mPin")
+                }else{
+                    dialog1 .dismiss()
+                }
+            }
+            btnreset.setOnClickListener {
+                etxt_oldpin.setText("")
+                etxt_newpin.setText("")
+                etxt_confirmnewpin.setText("")
+            }
+            dialog1.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    override fun onBackPressed() {
+        //super.onBackPressed()
+        quit()
     }
 
 }
