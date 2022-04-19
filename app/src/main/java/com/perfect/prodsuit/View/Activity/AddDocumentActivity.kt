6b8aa22f09ae5de4bbaed7560a96aa89f1db9b8c
Ranjithk <psstechteam@gmail.com>
2022-04-19 11:05:04 +0530

    HOME > LEAD > LEAD MANAGEMENT <> ADD DOCUMENT

---
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/AddDocumentActivity.kt
@@ -0,0 +1,41 @@
+package com.perfect.prodsuit.View.Activity
+
+import android.app.ProgressDialog
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
+import android.widget.ImageView
+import com.perfect.prodsuit.R
+
+class AddDocumentActivity : AppCompatActivity(), View.OnClickListener {
+
+    val TAG : String = "AddDocumentActivity"
+    lateinit var context: Context
+    private var progressDialog: ProgressDialog? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_add_document)
+        context = this@AddDocumentActivity
+
+        setRegViews()
+    }
+
+    private fun setRegViews() {
+        val imback = findViewById<ImageView>(R.id.imback)
+        imback!!.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.imback->{
+                finish()
+            }
+        }
+    }
+}
