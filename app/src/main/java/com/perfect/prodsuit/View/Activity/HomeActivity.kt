37a198fd4269ea6bc693df8deac4ada5d726df5b
perfectandroid <psstechteam@gmail.com>
2022-04-19 15:53:11 +0530

    REDESIGN PROJECT

--- app/src/main/java/com/perfect/prodsuit/View/Activity/HomeActivity.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/HomeActivity.kt
@@ -72,6 +72,7 @@ class HomeActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelect
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_homemain)
         setRegViews()
         bottombarnav()
