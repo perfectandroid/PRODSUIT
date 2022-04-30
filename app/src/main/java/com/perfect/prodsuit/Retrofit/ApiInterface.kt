396ec0257fbf31529fe385ad47ca78ef0eeaf20b
dilsha <psstechteam@gmail.com>
2022-04-30 10:19:38 +0530

    add quotation uploadS

--- app/src/main/java/com/perfect/prodsuit/Retrofit/ApiInterface.kt
+++ app/src/main/java/com/perfect/prodsuit/Retrofit/ApiInterface.kt
@@ -4,6 +4,7 @@ import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
 
@@ -140,8 +141,9 @@ interface ApiInterface {
     @POST("UserValidations/LeadGenerationList")
     fun getLeadGenerationList(@Body body: RequestBody): Call<String>
 
+
     @POST("UserValidations/AddQuatation")
-    fun getquotation(@Part body: RequestBody, @Part file: MultipartBody.Part): Call<String>
+    fun getquotation(@Body body: RequestBody): Call<String>
 
 
 
