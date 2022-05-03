dac05035a50bfacda307019ed2969ffcb4cc7fc6
Ranjithk <psstechteam@gmail.com>
2022-05-03 17:26:23 +0530

    HOME > DASH BOARD > BAR CHART LEADS STAGES COMPLETED

--- app/src/main/java/com/perfect/prodsuit/View/Activity/DashBoardActivity.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/DashBoardActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.*
 import androidx.lifecycle.ViewModelProvider
 import com.github.mikephil.charting.animation.Easing
@@ -92,6 +94,8 @@ class DashBoardActivity : AppCompatActivity() , View.OnClickListener{
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_dash_board)
         context = this@DashBoardActivity
 
