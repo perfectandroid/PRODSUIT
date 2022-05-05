39f37e3c3383b179002cd60332f4a8114156cccb
perfectandroid <psstechteam@gmail.com>
2022-05-05 11:37:46 +0530

    API INTEGRATION OF ADD & SEARCH EXPENSE

--- app/src/main/java/com/perfect/prodsuit/View/Activity/ExpenseActivity.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/ExpenseActivity.kt
@@ -1,13 +1,17 @@
 package com.perfect.prodsuit.View.Activity
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.view.Window
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -33,6 +37,7 @@ class ExpenseActivity : AppCompatActivity(), View.OnClickListener,ItemClickListe
     private var txtv_totexp: TextView?=null
     private var imexpense: ImageView?=null
     lateinit var expenseArrayList : JSONArray
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense)
@@ -64,7 +69,7 @@ class ExpenseActivity : AppCompatActivity(), View.OnClickListener,ItemClickListe
                 finish()
             }
             R.id.imexpense -> {
-                finish()
+                searchExpenseList()
             }
             R.id.fab -> {
                 val i = Intent(this@ExpenseActivity, ExpenseAddActivity::class.java)
@@ -72,6 +77,98 @@ class ExpenseActivity : AppCompatActivity(), View.OnClickListener,ItemClickListe
             }
         }
     }
+
+        private fun searchExpenseList() {
+            try {
+                val dialog1 = Dialog(this)
+                dialog1 .requestWindowFeature(Window.FEATURE_NO_TITLE)
+                dialog1 .setCancelable(false)
+                dialog1 .setContentView(R.layout.searchexpense_popup)
+                val txtfromDate = dialog1 .findViewById(R.id.txtfromDate) as TextView
+                val txttoDate = dialog1 .findViewById(R.id.txttoDate) as TextView
+                val txtok1 = dialog1 .findViewById(R.id.txtok1) as TextView
+                val txtok = dialog1 .findViewById(R.id.txtok) as TextView
+
+                val ll_Fromdate = dialog1 .findViewById(R.id.ll_Fromdate) as LinearLayout
+                val llfromdate = dialog1 .findViewById(R.id.llfromdate) as LinearLayout
+                val ll_Todate = dialog1 .findViewById(R.id.ll_Todate) as LinearLayout
+                val lltodate = dialog1 .findViewById(R.id.lltodate) as LinearLayout
+
+                val imclose = dialog1 .findViewById(R.id.imclose) as ImageView
+                val im_close = dialog1 .findViewById(R.id.im_close) as ImageView
+
+                val btn_Yes = dialog1 .findViewById(R.id.btn_Yes) as Button
+                val btn_No = dialog1 .findViewById(R.id.btn_No) as Button
+
+                val date_Picker1 = dialog1 .findViewById(R.id.date_Picker1) as DatePicker
+                val date_Picker = dialog1 .findViewById(R.id.date_Picker) as DatePicker
+
+                llfromdate.setOnClickListener {
+                    ll_Fromdate!!.visibility=View.VISIBLE
+                }
+
+                im_close.setOnClickListener {
+
+                    ll_Fromdate!!.visibility=View.GONE
+                }
+
+                lltodate.setOnClickListener {
+                    ll_Todate!!.visibility=View.VISIBLE
+                }
+
+                imclose.setOnClickListener {
+
+                    ll_Todate!!.visibility=View.GONE
+                }
+
+                txtok1.setOnClickListener {
+                    date_Picker1!!.minDate = Calendar.getInstance().timeInMillis
+                    val day: Int = date_Picker1!!.getDayOfMonth()
+                    val mon: Int = date_Picker1!!.getMonth()
+                    val month: Int = mon+1
+                    val year: Int = date_Picker1!!.getYear()
+                    var strDay = day.toString()
+                    var strMonth = month.toString()
+                    var strYear = year.toString()
+                    if (strDay.length == 1){
+                        strDay ="0"+day
+                    }
+                    if (strMonth.length == 1){
+                        strMonth ="0"+strMonth
+                    }
+                    txttoDate!!.setText(""+strDay+"-"+strMonth+"-"+strYear)
+                    ll_Todate!!.visibility=View.GONE
+                }
+                txtok.setOnClickListener {
+                    date_Picker!!.minDate = Calendar.getInstance().timeInMillis
+                    val day: Int = date_Picker!!.getDayOfMonth()
+                    val mon: Int = date_Picker!!.getMonth()
+                    val month: Int = mon+1
+                    val year: Int = date_Picker!!.getYear()
+                    var strDay = day.toString()
+                    var strMonth = month.toString()
+                    var strYear = year.toString()
+                    if (strDay.length == 1){
+                        strDay ="0"+day
+                    }
+                    if (strMonth.length == 1){
+                        strMonth ="0"+strMonth
+                    }
+                    txtfromDate!!.setText(""+strDay+"-"+strMonth+"-"+strYear)
+                    ll_Fromdate!!.visibility=View.GONE
+                }
+                btn_No.setOnClickListener {
+                    dialog1 .dismiss()
+                }
+                btn_Yes.setOnClickListener {
+                    dialog1.dismiss()
+                    getExpenses(txtfromDate.text.toString(), txttoDate.text.toString())
+                }
+                dialog1.show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
 
     companion object {
         var strfdate= ""
