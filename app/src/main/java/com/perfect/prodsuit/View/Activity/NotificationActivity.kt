35f6461bde475dd55ecf5003ae88856551fcfc12
Ranjithk <psstechteam@gmail.com>
2022-05-25 09:43:49 +0530

    HOME > REPORT > TICKET >  NEW LIST MVVM CREATE

--- app/src/main/java/com/perfect/prodsuit/View/Activity/NotificationActivity.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/NotificationActivity.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -29,6 +31,8 @@ class NotificationActivity : AppCompatActivity(), View.OnClickListener{
     lateinit var todoArrayList : JSONArray
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_notification)
         setRegViews()
         getNotificationList()
