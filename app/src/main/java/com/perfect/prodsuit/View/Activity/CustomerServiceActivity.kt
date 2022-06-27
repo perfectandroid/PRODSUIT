1fa17f9ba963b216fbb97c22194008ae10d3a71e
Ranjithk <psstechteam@gmail.com>
2022-06-27 11:06:10 +0530

    HOME > SERVICES > CUSTOMER SERVICE > COMPLAINT SCREEN DESIGN

--- app/src/main/java/com/perfect/prodsuit/View/Activity/CustomerServiceActivity.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/CustomerServiceActivity.kt
@@ -3,15 +3,29 @@ package com.perfect.prodsuit.View.Activity
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import android.widget.ImageView
+import android.widget.LinearLayout
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.perfect.prodsuit.R
 
 class CustomerServiceActivity : AppCompatActivity()  , View.OnClickListener {
 
+    val TAG : String = "CustomerServiceActivity"
     lateinit var context: Context
+
+    var til_CustomerNo: TextInputLayout? = null
+
+    var tie_CustomerNo: TextInputEditText? = null
+
+    private var tabLayout : TabLayout? = null
+    var llMainDetail: LinearLayout? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +35,90 @@ class CustomerServiceActivity : AppCompatActivity()  , View.OnClickListener {
         context = this@CustomerServiceActivity
 
         setRegViews()
+        addTabItem()
+
+        til_CustomerNo!!.setEndIconOnClickListener {
+            finish()
+        }
     }
+
+
 
     private fun setRegViews() {
         val imback = findViewById<ImageView>(R.id.imback)
         imback!!.setOnClickListener(this)
+
+        til_CustomerNo      = findViewById(R.id.til_CustomerNo)
+        tie_CustomerNo      = findViewById(R.id.tie_CustomerNo)
+
+
+
+        tabLayout      = findViewById(R.id.tabLayout)
+        llMainDetail = findViewById<LinearLayout>(R.id.llMainDetail)
+
+        til_CustomerNo!!.setOnClickListener(this)
+    }
+
+
+    private fun addTabItem() {
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Complaint"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Warranty"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Product"))
+        tabLayout!!.addTab(tabLayout!!.newTab().setText("Sales"))
+        tabLayout!!.tabMode = TabLayout.MODE_SCROLLABLE
+        getComplaints()
+        tabLayout!!.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                Log.e(TAG,"onTabSelected  113  "+tab.position)
+                if (tab.position == 0){
+                    Log.e(TAG,"onTabSelected  1131  "+tab.position)
+                    llMainDetail!!.removeAllViews()
+
+                  getComplaints()
+
+                }
+                if (tab.position == 1){
+                    Log.e(TAG,"onTabSelected  1131  "+tab.position)
+                    llMainDetail!!.removeAllViews()
+
+//                    getActivityDetails1()
+
+                }
+                if (tab.position == 2){
+                    Log.e(TAG,"onTabSelected  1131  "+tab.position)
+                    llMainDetail!!.removeAllViews()
+//                    getLocation()
+
+                }
+                if (tab.position == 3){
+                    Log.e(TAG,"onTabSelected  1131  "+tab.position)
+                    llMainDetail!!.removeAllViews()
+
+//                    getImages()
+                }
+                /*  if (tab.position == 4){
+                      Log.e(TAG,"onTabSelected  1131  "+tab.position)
+                      llMainDetail!!.removeAllViews()
+                   //   tv_actionType!!.visibility=View.GONE
+                      recyAgendaDetail!!.visibility=View.GONE
+                      getQuotationtails()
+                  }*/
+            }
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                Log.e(TAG,"onTabUnselected  162  "+tab.position)
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                Log.e(TAG,"onTabReselected  165  "+tab.position)
+            }
+        })
+    }
+
+    private fun getComplaints() {
+
+        val inflater = LayoutInflater.from(this@CustomerServiceActivity)
+        val inflatedLayout: View = inflater.inflate(R.layout.activity_customer_service_complaints, null, false)
+        llMainDetail!!.addView(inflatedLayout);
+
     }
 
     override fun onClick(v: View) {
@@ -33,6 +126,7 @@ class CustomerServiceActivity : AppCompatActivity()  , View.OnClickListener {
             R.id.imback->{
                 finish()
             }
+
         }
     }
 }
