bf30db8417237e05f70017652e3b0958a01a5460
Ranjithk <psstechteam@gmail.com>
2022-11-26 16:15:53 +0530

    Account Details > Message Design

--- app/src/main/java/com/perfect/prodsuit/View/Activity/AccountDetailsActivity.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Activity/AccountDetailsActivity.kt
@@ -8,6 +8,7 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.location.Address
 import android.location.Geocoder
 import android.location.Location
@@ -48,9 +49,9 @@ import com.ismaeldivita.chipnavigation.ChipNavigationBar
 import com.perfect.prodsuit.Helper.Config
 import com.perfect.prodsuit.Helper.ItemClickListener
 import com.perfect.prodsuit.R
-import com.perfect.prodsuit.Repository.ActivityListRepository
 import com.perfect.prodsuit.View.Adapter.*
 import com.perfect.prodsuit.Viewmodel.*
+import info.hoang8f.android.segmented.SegmentedGroup
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.ByteArrayInputStream
@@ -59,8 +60,9 @@ import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class AccountDetailsActivity : AppCompatActivity()  , View.OnClickListener, ItemClickListener,
-    OnMapReadyCallback {
+    OnMapReadyCallback, RadioGroup.OnCheckedChangeListener {
 
     internal var etdate: EditText? = null
     internal var ettime: EditText? = null
@@ -173,6 +175,13 @@ class AccountDetailsActivity : AppCompatActivity()  , View.OnClickListener, Item
     var imgv_2:ImageView?=null
     var crdv_1:CardView?=null
     var crdv_2:CardView?=null
+
+    private var messageType = "";
+    private var messageDesc = "";
+    private var cbWhat = "0";
+    private var cbEmail = "0";
+    private var cbMessage = "0";
+
 
     private lateinit var mMap: GoogleMap
     internal var mCurrLocationMarker: Marker? = null
@@ -728,11 +737,13 @@ class AccountDetailsActivity : AppCompatActivity()  , View.OnClickListener, Item
                 callFunction()
 
             }
+            R.id.ll_msg->{
+                messagePopup()
+            }
         }
     }
 
     private fun callFunction() {
-
 
         val ALL_PERMISSIONS = 101
 
@@ -845,12 +856,103 @@ class AccountDetailsActivity : AppCompatActivity()  , View.OnClickListener, Item
 
     private fun messagePopup() {
         try {
-            val builder = AlertDialog.Builder(this)
-            val inflater1 = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val layout = inflater1.inflate(R.layout.send_message_popup, null)
-            builder.setView(layout)
-            val alertDialog = builder.create()
-            alertDialog.show()
+
+            messageType = ""
+            cbWhat = "0"
+            cbEmail = "0"
+            cbMessage = "0"
+
+            val dialog1 = Dialog(this)
+            dialog1 .requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog1 .setCancelable(false)
+            dialog1 .setContentView(R.layout.send_message_popup)
+            dialog1.window!!.attributes.gravity = Gravity.CENTER;
+
+            val rbMessages = dialog1 .findViewById(R.id.rbMessages) as RadioButton
+            val rbReminder = dialog1 .findViewById(R.id.rbReminder) as RadioButton
+            val rbIntimation = dialog1 .findViewById(R.id.rbIntimation) as RadioButton
+
+            val edt_message = dialog1 .findViewById(R.id.edt_message) as EditText
+
+            val chk_whats = dialog1 .findViewById(R.id.chk_whats) as CheckBox
+            val chk_Email = dialog1 .findViewById(R.id.chk_Email) as CheckBox
+            val chk_Message = dialog1 .findViewById(R.id.chk_Message) as CheckBox
+
+            val btnMssubmit = dialog1 .findViewById(R.id.btnMssubmit) as Button
+            val btnMscancel = dialog1 .findViewById(R.id.btnMscancel) as Button
+
+            val segmented2 = dialog1 .findViewById(R.id.segmented2) as SegmentedGroup
+            segmented2.setTintColor(resources.getColor(R.color.color_msg_tab));
+            segmented2.setOnCheckedChangeListener(this@AccountDetailsActivity);
+
+            rbMessages.isChecked  =true
+            rbReminder.isChecked  =false
+            rbIntimation.isChecked  =false
+
+            chk_whats.setOnClickListener {
+                if (chk_whats.isChecked){
+
+                    cbWhat = "1"
+                }else{
+                    cbWhat = "0"
+                }
+            }
+
+            chk_Email.setOnClickListener {
+                if (chk_Email.isChecked){
+                    cbEmail = "1"
+                }else{
+                    cbEmail = "0"
+                }
+            }
+
+            chk_Message.setOnClickListener {
+
+                if (chk_Message.isChecked){
+                    cbMessage = "1"
+                }else{
+                    cbMessage = "0"
+                }
+            }
+
+            btnMscancel.setOnClickListener {
+                dialog1 .dismiss()
+            }
+
+            btnMssubmit.setOnClickListener {
+                messageDesc = edt_message.text.toString()
+                if (messageType.equals("")){
+
+                }
+                else if(messageDesc.equals("")){
+                    Config.snackBars(context,it,"Please enter message")
+//
+                }
+                else if (cbWhat.equals("0") && cbEmail.equals("0") && cbMessage.equals("0") ){
+                    Config.snackBars(context,it,"Please select sending options")
+//
+                }
+                else{
+                    Log.e(TAG,"  927  messageType  "+messageType)
+                    Log.e(TAG,"  927  messageDesc  "+messageDesc)
+                    Log.e(TAG,"  927  HHHHH  "+cbWhat+"  :   "+cbEmail+"  :  "+cbMessage)
+
+                    Config.Utils.hideSoftKeyBoard(context,it)
+                    dialog1 .dismiss()
+                    Toast.makeText(context,""+messageDesc,Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            dialog1.show()
+
+//            val builder = AlertDialog.Builder(this)
+//            val inflater1 = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//            val layout = inflater1.inflate(R.layout.send_message_popup, null)
+//
+//            var  ss =inflater1.findViewById(android.R.id.segmented2).setTintColor(Color.DKGRAY)
+//            builder.setView(layout)
+//            val alertDialog = builder.create()
+//            alertDialog.show()
         } catch (e: Exception) {
             e.printStackTrace()
         }
@@ -1874,4 +1976,24 @@ class AccountDetailsActivity : AppCompatActivity()  , View.OnClickListener, Item
         return BitmapFactory.decodeStream(arrayInputStream)
     }
 
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        when(checkedId){
+            R.id.rbMessages->{
+
+                messageType = "Message"
+                Log.e(TAG,"rbMessages")
+            }
+            R.id.rbReminder->{
+                Log.e(TAG,"rbReminder")
+                messageType = "Reminder"
+            }
+            R.id.rbIntimation->{
+                Log.e(TAG,"rbIntimation")
+                messageType = "Intimation"
+            }
+        }
+    }
+
+
 }
+
