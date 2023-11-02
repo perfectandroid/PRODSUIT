fd589a583a3c7c2f2637c99e1db188af67c3d998
dilsha <psstechteam@gmail.com>
2023-11-02 18:51:59 +0530

    Service assign employee and role validation corrected.

--- app/src/main/java/com/perfect/prodsuit/View/Adapter/DepartmentAdapter.kt
+++ app/src/main/java/com/perfect/prodsuit/View/Adapter/DepartmentAdapter.kt
@@ -37,6 +37,10 @@ class DepartmentAdapter(internal var context: Context, internal var jsonArray: J
                 val pos = position+1
                 holder.txtsino.text        = pos.toString()
                 holder.txtDepartment.text        = jsonObject!!.getString("DeptName")
+               /* if(pos==1)
+                {
+                    holder.txtDepartment.setText("yourString");
+                }*/
 //                if (position % 2 == 0){
 //                    holder.lldepartment!!.setBackgroundColor(context.getColor(R.color.greylight))
 //                }
