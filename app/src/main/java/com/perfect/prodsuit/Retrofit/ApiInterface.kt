38e4e681200423a5b46da5a164bd669d27f2d11b
perfectandroid <psstechteam@gmail.com>
2023-11-22 14:01:37 +0530

    Project Report

--- app/src/main/java/com/perfect/prodsuit/Retrofit/ApiInterface.kt
+++ app/src/main/java/com/perfect/prodsuit/Retrofit/ApiInterface.kt
@@ -113,6 +113,12 @@ interface ApiInterface {
     @POST("Report/ProjectReport")
     fun getProjectReport(@Body body: RequestBody): Call<String>
 
+    @POST("Report/ServiceNewList")
+    fun getServiceNewList(@Body body: RequestBody): Call<String>
+
+    @POST("Report/ProjectReportDetail")
+    fun getProjectReportDetail(@Body body: RequestBody): Call<String>
+
     @POST("Project/WorkTypeDetails")
     fun getWorkTypeDetails(@Body body: RequestBody): Call<String>
 
