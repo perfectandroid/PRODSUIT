97cd510d040fa4ca5a384cf7aad4fcfed04a413a
sarath <psstechteam@gmail.com>
2023-11-29 19:00:16 +0530

    lead graph design changes text

---
+++ app/src/main/java/com/perfect/prodsuit/Repository/LeadCountFollowupRepository.kt
@@ -0,0 +1,137 @@
+package com.perfect.prodsuit.Repository
+
+import android.app.ProgressDialog
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import com.google.gson.GsonBuilder
+import com.perfect.prodsuit.Api.ApiInterface
+import com.perfect.prodsuit.Helper.Config
+import com.perfect.prodsuit.Helper.ProdsuitApplication
+import com.perfect.prodsuit.Model.LeadCountFollowupModel
+import com.perfect.prodsuit.Model.LeadTileModel
+import com.perfect.prodsuit.Model.ServiceCountModel
+import com.perfect.prodsuit.R
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody
+import org.json.JSONObject
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.text.SimpleDateFormat
+import java.util.*
+
+object LeadCountFollowupRepository {
+
+    private var progressDialog: ProgressDialog? = null
+    val leadCountFollowupSetterGetter = MutableLiveData<LeadCountFollowupModel>()
+    val TAG: String = "LeadCountFollowupRepository"
+
+    fun getServicesApiCall(context: Context): MutableLiveData<LeadCountFollowupModel> {
+        getLeadCountFollowup(context)
+        return leadCountFollowupSetterGetter
+    }
+
+    private fun getLeadCountFollowup(context: Context) {
+        try {
+            leadCountFollowupSetterGetter.value = LeadCountFollowupModel("")
+            val BASE_URLSP = context.getSharedPreferences(Config.SHARED_PREF7, 0)
+            progressDialog = ProgressDialog(context, R.style.Progress)
+            progressDialog!!.setProgressStyle(android.R.style.Widget_ProgressBar)
+            progressDialog!!.setCancelable(false)
+            progressDialog!!.setIndeterminate(true)
+            progressDialog!!.setIndeterminateDrawable(context.resources.getDrawable(
+                R.drawable.progress))
+            progressDialog!!.show()
+            val client = OkHttpClient.Builder()
+                .sslSocketFactory(Config.getSSLSocketFactory(context))
+                .hostnameVerifier(Config.getHostnameVerifier())
+                .build()
+            val gson = GsonBuilder()
+                .setLenient()
+                .create()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URLSP.getString("BASE_URL", null))
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .client(client)
+                .build()
+            val apiService = retrofit.create(ApiInterface::class.java!!)
+            val requestObject1 = JSONObject()
+            try {
+
+                val BankKeySP = context.getSharedPreferences(Config.SHARED_PREF9, 0)
+                val TokenSP = context.getSharedPreferences(Config.SHARED_PREF5, 0)
+                val FK_EmployeeSP = context.getSharedPreferences(Config.SHARED_PREF1, 0)
+                val FK_DepartmentSP = context.getSharedPreferences(Config.SHARED_PREF55, 0)
+                val Fkcompanysp = context.getSharedPreferences(Config.SHARED_PREF39, 0)
+                val EntrBySP = context.getSharedPreferences(Config.SHARED_PREF36, 0)
+                val FK_BranchSP = context.getSharedPreferences(Config.SHARED_PREF37, 0)
+                val FK_BranchCodeUserSP = context.getSharedPreferences(Config.SHARED_PREF40, 0)
+
+             /*   requestObject1.put("BankKey", ProdsuitApplication.encryptStart(BankKeySP.getString("BANK_KEY", null)))
+                requestObject1.put("ReqMode", ProdsuitApplication.encryptStart("15"))*/
+                val sdf = SimpleDateFormat("yyyy-MM-dd")
+                val currentDate = sdf.format(Date())
+                System.out.println(" C DATE is  "+currentDate)
+
+                requestObject1.put("BankKey", ProdsuitApplication.encryptStart(BankKeySP.getString("BANK_KEY", null)))
+                requestObject1.put("Token", ProdsuitApplication.encryptStart(TokenSP.getString("Token", null)))
+
+              requestObject1.put("FK_Employee", ProdsuitApplication.encryptStart(FK_EmployeeSP.getString("FK_Employee", null)))
+              //  requestObject1.put("FK_Employee", ProdsuitApplication.encryptStart("40"))
+                requestObject1.put("EntrBy", ProdsuitApplication.encryptStart(EntrBySP.getString("UserCode", null)))
+                requestObject1.put("FK_Department", ProdsuitApplication.encryptStart(FK_DepartmentSP.getString("FK_Department", null)))
+                requestObject1.put("FK_Branch", ProdsuitApplication.encryptStart(FK_BranchSP.getString("FK_Branch", null)))
+                requestObject1.put("FK_Company", ProdsuitApplication.encryptStart(Fkcompanysp.getString("FK_Company", null)))
+                requestObject1.put("FK_BranchCodeUser", ProdsuitApplication.encryptStart(FK_BranchCodeUserSP.getString("FK_BranchCodeUser", null)))
+                requestObject1.put("TransDate", ProdsuitApplication.encryptStart(currentDate))
+                requestObject1.put("DashMode", ProdsuitApplication.encryptStart("8"))
+                requestObject1.put("DashType", ProdsuitApplication.encryptStart("1"))
+
+
+
+                Log.e(TAG,"78  getTile  "+requestObject1)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            val body = RequestBody.create(
+                okhttp3.MediaType.parse("application/json; charset=utf-8"),
+                requestObject1.toString()
+            )
+
+           // val call = apiService.getDashboardDetails(body)
+            val call = apiService.getDashboardTileDetails(body)
+            call.enqueue(object : retrofit2.Callback<String> {
+                override fun onResponse(
+                    call: retrofit2.Call<String>, response:
+                    Response<String>
+                ) {
+                    try {
+                        progressDialog!!.dismiss()
+                        val jObject = JSONObject(response.body())
+                        Log.e(TAG,"  LEAD SCENARIO "+response.body())
+                        val leads = ArrayList<LeadTileModel>()
+                        leads.add(LeadTileModel(response.body()))
+                        val msg = leads[0].message
+                        leadCountFollowupSetterGetter.value = LeadCountFollowupModel(msg)
+                    } catch (e: Exception) {
+                        progressDialog!!.dismiss()
+                        Toast.makeText(context,""+ Config.SOME_TECHNICAL_ISSUES, Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onFailure(call: retrofit2.Call<String>, t: Throwable) {
+                    progressDialog!!.dismiss()
+                    Toast.makeText(context,""+ Config.SOME_TECHNICAL_ISSUES, Toast.LENGTH_SHORT).show()
+                }
+            })
+        }catch (e : Exception){
+            e.printStackTrace()
+            Toast.makeText(context,""+ Config.SOME_TECHNICAL_ISSUES, Toast.LENGTH_SHORT).show()
+           progressDialog!!.dismiss()
+        }
+    }
+
+}
