626ad5cd67aa6e3963f216b547e003ddd3deea85
Ranjithk <psstechteam@gmail.com>
2024-01-05 14:53:06 +0530

    05-01-2024 -3:00 Other Charge Calculation in Project Transaction

--- app/src/main/java/com/perfect/prodsuit/Viewmodel/UpdateProjectTransactionViewModel.kt
+++ app/src/main/java/com/perfect/prodsuit/Viewmodel/UpdateProjectTransactionViewModel.kt
@@ -13,9 +13,10 @@ class UpdateProjectTransactionViewModel : ViewModel()  {
     var updateProjectTransactionData: MutableLiveData<UpdateProjectTransactionModel>? = null
     fun SaveUpdateProjectTransaction(context: Context,UserAction : String,Date : String,FK_Project : String,FK_Stage : String,
                                  NetAmount : String,OtherCharge : String,Remark : String,
-                                 pssOtherCharge : JSONArray ,pssOtherChargeTax : JSONArray,PaymentDetails : JSONArray) : LiveData<UpdateProjectTransactionModel>? {
+                                 pssOtherCharge : JSONArray ,pssOtherChargeTax : JSONArray,PaymentDetails : JSONArray,
+                                     ID_TransactionType : String,ID_Employee : String,strRoundOff : String,ID_BillType : String,ID_PettyCashier : String) : LiveData<UpdateProjectTransactionModel>? {
         updateProjectTransactionData = UpdateProjectTransactionRepository.getServicesApiCall(context,UserAction,Date,FK_Project,FK_Stage,NetAmount,OtherCharge,
-            Remark,pssOtherCharge,pssOtherChargeTax,PaymentDetails)
+            Remark,pssOtherCharge,pssOtherChargeTax,PaymentDetails,ID_TransactionType,ID_Employee,strRoundOff,ID_BillType,ID_PettyCashier)
         return updateProjectTransactionData
     }
 }
